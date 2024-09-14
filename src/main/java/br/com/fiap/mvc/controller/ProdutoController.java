66f5e85d0226dac87c3b3e1527e07fede775db5a
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-09-13 21:43:39 -0300

    Estilizando com Bootstrap e realizando o cadastro de um Produto

--- src/main/java/br/com/fiap/mvc/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/mvc/controller/ProdutoController.java
@@ -1,6 +1,9 @@
 package br.com.fiap.mvc.controller;
 
+import br.com.fiap.mvc.model.Produto;
+import br.com.fiap.mvc.repositories.ProdutoRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("produto")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository repository;
+
     @GetMapping("cadastrar")
-    public String cadastrarProduto() {
+    public String cadastrarProduto(Model model) {
+        model.addAttribute("produto", new Produto());
         return "produto/cadastrar";
     }
 
     @PostMapping("cadastrar")
     @Transactional
-    public String post(String nome, Double valor, Model model) {
-        model.addAttribute("nome", nome);
-        model.addAttribute("valor", valor);
+    public String post(Produto produto, Model model) {
+        repository.save(produto);
         model.addAttribute("msg", "Produto cadastrado!");
         return "produto/cadastrar";
     }
---
+++ src/main/java/br/com/fiap/mvc/model/Produto.java
@@ -0,0 +1,35 @@
+package br.com.fiap.mvc.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "JA_PRODUTO")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Produto {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_PRODUTO")
+    private Long id;
+
+    @Column(name = "NM_PRODUTO", length = 200, nullable = false)
+    private String nome;
+
+    @Column(name = "VL_PRODUTO", nullable = false)
+    private Double valor;
+
+    @Column(name = "DS_PRODUTO", length = 500, nullable = false)
+    private String descricao;
+
+    @Column(name = "DT_FABRICACAO", nullable = false)
+    private LocalDate dataDeFabricacao;
+
+    @Column(name = "ST_NOVO", nullable = false)
+    private Boolean novo;
+}
