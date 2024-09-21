95bd31c8284c83e27fa20dd60d0433cd2d74dac9
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-09-20 21:07:53 -0300

    Criação da página de listagem do produtos e iniciando a criação da página de atualização

--- src/main/java/br/com/fiap/mvc/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/mvc/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,5 +30,17 @@ public class ProdutoController {
         repository.save(produto);
         model.addAttribute("msg", "Produto cadastrado!");
         return "produto/cadastrar";
+    }
+
+    @GetMapping("listar")
+    public String listarProdutos(Model model) {
+        model.addAttribute("produtos", repository.findAll());
+        return "produto/listar";
+    }
+
+    @GetMapping("editar/{id}")
+    public String editar(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("produto", repository.findById(id));
+        return "produto/editar";
     }
 }
